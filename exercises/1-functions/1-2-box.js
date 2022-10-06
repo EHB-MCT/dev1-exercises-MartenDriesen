e3dae7b832cab61f1b0088cc9106e19a6ee37d07
Marten <marten.driesen@student.ehb.be>
2022-10-06 10:15:00 +0200

    Javascript

--- exercises/1-functions/1-2-box.js
+++ exercises/1-functions/1-2-box.js
@@ -12,13 +12,24 @@ function drawLine() {
     context.lineWidth = 7;
 
     //box
-    context.strokeStyle = 'blue';
+    context.strokeStyle = 'black';
     context.beginPath();
     context.moveTo(50, 50);
     context.lineTo(200, 50);
     context.lineTo(200, 200);
     context.lineTo(50, 200);
+    context.lineTo(50, 50);
     context.stroke();
+
+    //cross
+    context.strokeStyle = 'red';
+    context.beginPath();
+    context.moveTo(50, 50);
+    context.lineTo(200, 200);
+    context.moveTo(200, 50);
+    context.lineTo(50, 200);
+    context.stroke();
+
 
 
 
