de28233abc2fc4e6a9f5e197f700a7d6f924e7ca
Marten <marten.driesen@student.ehb.be>
2022-11-13 00:03:01 +0100

    meer iteration2 oef

--- exercises/4-iteration2/4-4-2circles.js
+++ exercises/4-iteration2/4-4-2circles.js
@@ -13,8 +13,11 @@ function drawcircle() {
     for (let j = 0; j < 10; j++) {
 
         for (let i = 0; i < 10; i++) {
+
             let size = Math.random() * 40;
+
             context.fillStyle = Utils.rgba(173, 216, 230, 50);
+
             Utils.fillCircle(0 + (50 * i), 0 + (50 * j), size);
         }
     }
